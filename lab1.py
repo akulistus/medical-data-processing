bb4a76b2ed4a206839961374da0c8462d40f40af
Keifrick Bateman <akulistus555@gmail.com>
2023-09-16 17:43:43 +0300

    fixed lab1-lab3 in order to meet pip8

--- lab1.py
+++ lab1.py
@@ -1,13 +1,14 @@
 import numpy as np
-import matplotlib.pyplot as plt
-mean1 = 5
-std1 = 2
-N = 10
-x1 = np.random.normal(mean1, std1, [1, N])
 
-mean2 = 2
-std2 = 1
-x2 = np.random.normal(mean2, std2, [1, N])
+mean = 5
+std = 2
+N = 10
+x1 = np.random.normal(mean, std, [1, N])
+
+mean = 2
+std = 1
+x2 = np.random.normal(mean, std, [1, N])
+
 print(x1)
 print(x2)
 x1_mean = np.mean(x1)
@@ -15,8 +16,8 @@ x2_mean = np.mean(x2)
 x1_std = np.std(x1)
 x2_std = np.std(x2)
 c = np.corrcoef(x1, x2)
-print(x1_mean)
-print(x2_mean)
-print(x1_std)
-print(x2_std)
+print(f"{x1_mean=}")
+print(f"{x2_mean=}")
+print(f"{x1_std=}")
+print(f"{x2_std=}")
 print(c)
--- lab3.py
+++ lab3.py
@@ -3,7 +3,7 @@ import fisher_func
 import matplotlib.pyplot as plt
 
 
-#Пример
+# Пример
 # data = load_iris()
 # X = data.data
 # Y = data.target
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 # plt.scatter(setosa_data[:, 0], setosa_data[:, 1])
 # plt.show()
 
-#Задние
+# Задние
 
 prty_1 = 2
 prty_2 = 3
@@ -23,25 +23,34 @@ prty_2 = 3
 iris_data = fisher_func.get_irises()
 Y_str = iris_data.keys()
 
-fig, (ax1,ax2,ax3) = plt.subplots(1,3)
-ax1.scatter(iris_data["setosa"][:,prty_1],iris_data["setosa"][:,prty_2])
-ax2.scatter(iris_data["versicolor"][:,prty_1],iris_data["versicolor"][:,prty_2])
-ax3.scatter(iris_data["virginica"][:,prty_1],iris_data["virginica"][:,prty_2])
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+scatter_data_1 = iris_data["setosa"][:, prty_1]
+scatter_data_2 = iris_data["setosa"][:, prty_2]
+ax1.scatter(scatter_data_1, scatter_data_2)
+
+scatter_data_1 = iris_data["versicolor"][:, prty_1]
+scatter_data_2 = iris_data["versicolor"][:, prty_2]
+ax2.scatter(scatter_data_1, scatter_data_2)
+
+scatter_data_1 = iris_data["virginica"][:, prty_1]
+scatter_data_2 = iris_data["virginica"][:, prty_2]
+ax3.scatter(scatter_data_1, scatter_data_2)
 plt.show()
 
 flowers_info = dict()
 for name in Y_str:
-    flower_mean = (np.mean(iris_data[name][:,prty_1]), np.mean(iris_data[name][:,prty_2]))
-    flower_std = (np.std(iris_data[name][:,prty_1]), np.std(iris_data[name][:,prty_2]))
-    flower_xcor = np.corrcoef(iris_data[name][:,prty_1], iris_data[name][:,prty_2])
-    flowers_info[name] = [flower_mean,flower_std,flower_xcor]
+    data_1 = iris_data[name][:, prty_1]
+    data_2 = iris_data[name][:, prty_2]
 
-fig, (ax1,ax2) = plt.subplots(1,2)
-ax1.hist(iris_data["setosa"][:,prty_1])
-ax1.hist(iris_data["setosa"][:,prty_2])
-data1 = iris_data["setosa"][:,prty_1]
-data2 = iris_data["setosa"][:,prty_2]
-ax2.boxplot([data1,data2])
+    flower_mean = (np.mean(data_1), np.mean(data_2))
+    flower_std = (np.std(data_1), np.std(data_2))
+    flower_xcor = np.corrcoef(data_1, data_1)
+    flowers_info[name] = [flower_mean, flower_std, flower_xcor]
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.hist(iris_data["setosa"][:, prty_1])
+ax1.hist(iris_data["setosa"][:, prty_2])
+data1 = iris_data["setosa"][:, prty_1]
+data2 = iris_data["setosa"][:, prty_2]
+ax2.boxplot([data1, data2])
 plt.show()
-
-# print(flowers_info["virginica"])
