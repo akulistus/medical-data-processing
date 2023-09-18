19128992bd1ae3b9ff681022f27c25ec99e0df4f
akulistus <8carat@rambler.ru>
2023-09-18 14:40:23 +0300

    initial lab4 realisation

--- labs/lab4.py
+++ labs/lab4.py
@@ -1,13 +1,21 @@
 import numpy as np
-import fisher_func
+import src.fisher_func as fisher_func
+import matplotlib.pyplot as plt
 
 iris_data = fisher_func.get_irises()
 
 class1 = "versicolor"
-class2 = "virginica"
+class2 = "setosa"
 
 data_class1 = iris_data[class1]
 data_class2 = iris_data[class2]
+test = np.vstack([data_class1, data_class2])
+print(test)
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.scatter(test[:,1], test[:,2], test[:,3])
+plt.show()
 
 diff_mean = np.mean(data_class1, axis=0) - np.mean(data_class2, axis=0)
 sum_covariance = np.cov(data_class1, rowvar=0) + np.cov(data_class2, rowvar=0)
@@ -16,3 +24,9 @@ w = W/np.linalg.norm(W)
 
 proj_class1 = np.matmul(data_class1, w)
 proj_class2 = np.matmul(data_class2, w)
+
+print(proj_class1)
+hist_data = np.hstack((proj_class1, proj_class2))
+print(hist_data)
+plt.hist(hist_data)
+plt.show()
