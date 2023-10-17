77f1ceeb724a1e03cfb15ca20025d40dc111dd5d
Keifrick Bateman <akulistus555@gmail.com>
2023-10-18 02:05:06 +0300

    fixed lab6

--- labs/lab5.py
+++ labs/lab5.py
@@ -3,15 +3,37 @@ import src.fisher_func as fisher_func
 import matplotlib.pyplot as plt
 from sklearn.decomposition._pca import PCA
 
-PCA_obj = PCA(n_components=2)
+PCA_obj = PCA()
 
-data = fisher_func.get_irises()
+data, Y = fisher_func.get_irises()
 
 data_setosa = data["setosa"]
-data_versicolor = data["virginica"]
-data = np.vstack((data_setosa, data_versicolor))
+data_virginica = data["virginica"]
+data_versicolor = data["versicolor"]
 
-reduced_data = PCA_obj.fit_transform(data)
+data_svi = np.vstack((data_setosa, data_virginica))
+data_sve = np.vstack((data_setosa, data_versicolor))
+data_vevi = np.vstack((data_versicolor, data_virginica))
+reduced_data_svi = PCA_obj.fit_transform(data_svi)
+print(reduced_data_svi)
+# reduced_data_sve = PCA_obj.fit_transform(data_sve)
+# reduced_data_vevi = PCA_obj.fit_transform(data_vevi)
 
-plt.scatter(reduced_data[:,0],reduced_data[:,1])
-plt.show()
+# plt.scatter(reduced_data_svi[:,0],reduced_data_svi[:,1])
+# plt.xlabel('Первая главная компонента')
+# plt.ylabel('Вторая главная компонента')
+# plt.title('setosa и virginica в пространстве полученных главных компонент')
+# plt.show()
+
+# plt.scatter(reduced_data_sve[:,0],reduced_data_sve[:,1])
+# plt.xlabel('Первая главная компонента')
+# plt.ylabel('Вторая главная компонента')
+# plt.title('setosa и versicolor в пространстве полученных главных компонент')
+# plt.show()
+
+# plt.scatter(reduced_data_vevi[0:49,0],reduced_data_vevi[0:49,1], marker='o')
+# plt.scatter(reduced_data_vevi[50:99,0],reduced_data_vevi[50:99,1], marker='*')
+# plt.xlabel('Первая главная компонента')
+# plt.ylabel('Вторая главная компонента')
+# plt.title('versicolor и virginica в пространстве полученных главных компонент')
+# plt.show()
--- labs/lab6.py
+++ labs/lab6.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy as cp
 import matplotlib.pyplot as plt
 
 gr1 = np.array([[0.7,   0.3,   1.2],
@@ -24,37 +23,43 @@ gr2 = np.array([[0.4,   0.2,   0.8],
 [0.3,   0.2,   0.6],
 [0.5,   0.5,   0.8]])
 
-# gr2 = gr2*-1
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 
-# diff_mean = np.mean(gr1, axis=0) - np.mean(gr2, axis=0)
-# sum_covariance = np.cov(gr1, rowvar=0) + np.cov(gr2, rowvar=0)
-# W = np.matmul(np.linalg.inv(sum_covariance), diff_mean)
-# print(W)
-# w = W/np.linalg.norm(W)
+ax.scatter(gr1[:,0], gr1[:,1], gr1[:,2])
+ax.scatter(gr2[:,0], gr2[:,1], gr2[:,2])
+plt.show()
 
+ones1 = np.ones((10,1))
+ones2 = np.ones((10,1))
+gr1_1 = np.hstack((gr1, ones1))
+gr2_2 = np.hstack((gr2, ones2))
+gr2_2 = gr2_2*-1
 
-# proj_class1 = np.matmul(data, w)
+diff_mean = np.mean(gr1_1, axis=0) - np.mean(gr2_2, axis=0)
+sum_covariance = np.cov(gr1_1, rowvar=0) + np.cov(gr2_2, rowvar=0)
+W = np.matmul(np.linalg.pinv(sum_covariance), diff_mean)
+print(W)
+w = W/np.linalg.norm(W)
 
-# plt.hist(proj_class1)
-# plt.show()
+data = np.vstack((gr1_1,gr2_2))
+projections = np.matmul(data, w)
 
-data = np.vstack((gr1, gr2*-1))
-# Set learning rate to 0.1
 learn_coeff = 0.1
-projections = np.zeros(20)
-projections[1] = -1
-w = [18,52,21]
-
 while min(projections) < 0:
-    # Test all data and tweak the weights
     for ind, data_sample in enumerate(data):
         projections[ind] = np.matmul(data_sample, w)
         if projections[ind] < 0:
             w = w + data_sample * learn_coeff
-
-    print(min(projections))
-    # Recalculate projections based on last value of w for the current iteration
     projections = np.matmul(data, w)
 
-plt.hist(projections)
+test = np.matmul(gr1_1, w)
+gr2_2 = gr2_2*-1
+test1 = np.matmul(gr2_2, w)
+
+plt.hist(test)
+plt.hist(test1)
+plt.xlabel("Проекции")
+plt.ylabel("N")
+plt.title("Гистограмма проекций двух классов")
 plt.show()
